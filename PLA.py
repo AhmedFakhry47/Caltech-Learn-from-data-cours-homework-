ec775da85aa928b27c39f295228084f67cbaae76
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2020-03-22 03:54:29 +0200

    Update PLA.py

--- PLA.py
+++ PLA.py
@@ -3,15 +3,17 @@ A simple code (Script) to illustrate the idea behind perception
 learning algorithm
 '''
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 ##First step is generating data
 D = np.random.rand(3,1000)
-threshold = .4 
+threshold = .4
 
 
 #target function represented in weights 
 f  	    =  np.array([0.2,0.8,0.5]).reshape((3,1))
-Y  	    = (np.sum((f*D),axis=0) > threshold).astype(int) 
+Y 			=  (np.sum((f*D),axis=0)>threshold).astype(int)
 Y[Y==0] = -1
 
 
@@ -27,17 +29,4 @@ for _ in range(10000):
 		if (h != Y[i]):	#Update step in case of miss-classified points 
 			W+= Y[i]*D[:,i]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(W)
